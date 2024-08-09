72beaf89a5dc4ab6bc264f772b723b509a5e34b0
Nathan Mwai <nathan.mwai@student.moringaschool.com>
2024-08-09 20:50:17 +0300

    Why it passes I have no idea

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+
+  const [questions, setQuestions] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+    .then(res => res.json())
+    .then(questions => setQuestions(questions))
+  },[])
+
+  function handleDeleteButton(id){
+    fetch(`http://localhost:4000/questions/${id}`,{
+      method: "DELETE",
+    })
+    .then(r => r.json())
+    .then(() => {
+      const newQuestions = questions.filter((quiz) => quiz.id !== id);
+    setQuestions(newQuestions);
+  })
+  }
+
+  
+
+  const questionItems = questions.map((quiz) => (
+    <QuestionItem
+      key={quiz.id}
+      question={quiz}
+      onDeleteQuestion={handleDeleteButton}
+      // onAnswerChange={handleAnswerChange}
+    />
+  ));
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{questionItems}</ul>
     </section>
   );
 }
